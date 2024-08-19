8f206b98dccec31a0b7cf1bb53cebe78735ff436
Alexandre <alexandre.hernandez@yahoo.com>
2024-08-20 00:25:28 +0200

    20/08/2024

--- app/(pages)/collection-joaillerie/[article]/page.tsx
+++ app/(pages)/collection-joaillerie/[article]/page.tsx
@@ -99,7 +99,7 @@ export default function ArticlePage({ params }: { params: { article: string } })
             <footer className="bg-gray-800 text-white py-4">
                 <div className="container mx-auto text-center">
                     <p>&copy; 2024 Éminence. Tous droits réservés.</p>
-                    <p>Développé par <Link className="underline" href="https://www.linkedin.com/in/alexandre-hernandez-392603309/">Alexandre Hernandez</Link></p>
+                    <p>Développé par <Link className="underline" href="https://www.linkedin.com/in/alexandre-hernandez-392603309/">Alexandre Hernandez </Link></p>
                 </div>
             </footer>
         </>
